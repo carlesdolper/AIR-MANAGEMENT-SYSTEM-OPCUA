8d9648e9834c98cec17ccb2110ed467c2861d400
carlesdolper <carlesdolper@gmail.com>
2024-12-20 11:00:10 -0600

    test

--- analysis.py
+++ analysis.py
@@ -4,7 +4,6 @@ import numpy as np
 from datetime import datetime, timedelta
 from pathlib import Path
 
-#tewst
 class Analyzer:
     def __init__(self):
         # Usar Path para manejar rutas de manera segura
